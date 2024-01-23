e36499705697a814d9fc6d5aaedfcb7ceba2e5c6
lidia-27 <156946457+lidia-27@users.noreply.github.com>
2024-01-23 11:12:29 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
-const sidebar=document.querySelector('.sidebar')
-const btn =document.querySelector('button')
+let play = document.querySelector('.play')
+let pause = document.querySelector('.pause')
+let restart = document.querySelector('.restart')
 
-btn.addEventListner('click', () => {
-    sidebar.classList.toggle(hide);
+let tweeen = gsap.to(".box1",{
+y: -200,
+duration: 5,
+backgroundColor: "teal",
 });
+
+
+play.addEventListener( 'Click', ()=> tween.play());
+pause.addEventListener( 'Click', ()=> tween.pause());
+restart.addEventListener( 'Click', ()=> tween.restart());
+
+
+
